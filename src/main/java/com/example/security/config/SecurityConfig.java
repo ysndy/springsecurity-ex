2b746132fc91e0e08d267dd28725a8b38703c646
ysndy <ysndy@naver.com>
2023-10-08 18:55:43 +0900

    Basic Authentication

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.security.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(
+                        (requests) -> requests
+                                .anyRequest()
+                                .authenticated()
+                );
+        http.httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+
+}
--- src/main/java/com/example/security/controllers/GreetingsController.java
+++ src/main/java/com/example/security/controllers/GreetingsController.java
@@ -1,4 +1,4 @@
-package com.example.security;
+package com.example.security.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
